ecc2b647359f9caa4e0a5cb737f0da0c60ce88b8
rovms <romabucher.afc@gmail.com>
2020-04-26 10:37:14 +0200

    renaming

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,16 +3,15 @@
     <h3>What would you like to see?</h3>
     <v-list>
       <v-list-item>
-        <v-btn small :to="{ name: 'consumption' }">See reading in table</v-btn>
+        <v-btn small :to="{ name: 'consumption' }">Readings in table</v-btn>
       </v-list-item>
       <v-list-item>
-        <v-btn small :to="{ name:  'consumption_chart' }">See development chart</v-btn>
+        <v-btn small :to="{ name: 'consumption_chart' }"
+          >Development chart</v-btn
+        >
       </v-list-item>
       <v-list-item>
-        <v-btn small :to="{ name:  'consumption_weekly' }">See daily totals</v-btn>
-      </v-list-item>
-      <v-list-item>
-        <v-btn small :to="{ name:  'consumption_time' }">See hourly averages</v-btn>
+        <v-btn small :to="{ name: 'consumption_time' }">Hourly averages</v-btn>
       </v-list-item>
     </v-list>
   </v-container>
@@ -23,6 +22,6 @@ export default {
   name: "Dashboard",
   data() {
     return {};
-  }
+  },
 };
 </script>
